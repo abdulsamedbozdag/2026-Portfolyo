2268cf97892e92cda3ebfea1504b98fa6b49316e
abdulsamedbozdag <86298352+abdulsamedbozdag@users.noreply.github.com>
2026-02-28 17:20:50 +0300

    fix: restore Link import and refine Prometeon slogan styles

--- src/components/editorial/PrometeonInterlude.tsx
+++ src/components/editorial/PrometeonInterlude.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Link from "next/link";
 import { TireScene } from "@/components/prometeon/TireScene";
 import Image from "next/image";
 
